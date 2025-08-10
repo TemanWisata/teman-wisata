ed5880deadf881ffd5dfe01c640ed94530fb0c25
pradanaadn <pradanaadn21@gmail.com>
2025-08-10 21:52:39 +0800

    feat: initial layout for teman-wisata

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,16 +1,31 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import handlebars from 'vite-plugin-handlebars';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'node:path';
 export default defineConfig({
-    plugins: [
-        tailwindcss(),
-    ],
-    server: {
-        proxy: {
-            "/api": {
-                target: "https://jsonplaceholder.typicode.com",
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, '')
-            }
-        }
-    }
-})
+  plugins: [
+    tailwindcss(),
+    handlebars({
+      partialDirectory: path.resolve(__dirname, 'component'),
+    }),
+  ],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, 'index.html'),
+        recommender: path.resolve(__dirname, 'recommender.html'),
+        place: path.resolve(__dirname, 'place.html'),
+      },
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://jsonplaceholder.typicode.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
